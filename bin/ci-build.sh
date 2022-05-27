fa7c65675be00a1ccb4d4ab2addf7e87d3b431b9
Azeem Sajid <azeem.sajid@gmail.com>
2022-05-27 09:31:24 +0500

    Add and configure bin/ci-build.sh script.

---
+++ bin/ci-build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+if [ "$PREFIX" = "" ] || [ "$CC" = "" ] || [ "$MAKE" = "" ]; then
+  echo "[ERR] Environment variables are not set!"
+  echo "[ERR] Set PREFIX, CC, and MAKE before running $0 script."
+  exit 1
+fi
+
+echo "PREFIX: $PREFIX"
+echo "CC:     $CC"
+echo "MAKE:   $MAKE"
+
+echo "Building [$PREFIX]"
+
+"$CC" --version
+./configure --prefix="$PREFIX"
+"$MAKE" install
+
+echo "Compressing"
+cd "$PREFIX"
+zip -r "$PREFIX.zip" .
+cd ..
+
+rm -rf build "$PREFIX"
+
+echo "Listing"
+tree -h "$PREFIX"
+mv "$PREFIX/$PREFIX.zip" .
+
+echo "--- [DONE] ---"
