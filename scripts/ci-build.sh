53a05582a5362e943190d711bad4d273e62bfe10
Azeem Sajid <azeem.sajid@gmail.com>
2022-05-28 15:42:02 +0500

    Use separate directory artifacts.

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-if [ "$PREFIX" = "" ] || [ "$CC" = "" ] || [ "$MAKE" = "" ]; then
+if [ "$ARTIFACT_DIR" = "" ] || [ "$PREFIX" = "" ] || [ "$CC" = "" ] || [ "$MAKE" = "" ]; then
   echo "[ERR] One or more environment variable(s) are not set!"
   echo "[ERR] Set PREFIX, CC, and MAKE before running $0 script."
   exit 1
 fi
 
-if [ "$TESTS" != true ]; then
-  TESTS=false
+if [ "$RUN_TESTS" != true ]; then
+  RUN_TESTS=false
 fi
 
-echo "[INF] PREFIX: $PREFIX"
-echo "[INF] CC:     $CC"
-echo "[INF] MAKE:   $MAKE"
-echo "[INF] TESTS:  $TESTS"
+echo "[INF] ARTIFACT_DIR: $ARTIFACT_DIR"
+echo "[INF] PREFIX:       $PREFIX"
+echo "[INF] CC:           $CC"
+echo "[INF] MAKE:         $MAKE"
+echo "[INF] RUN_TESTS:    $RUN_TESTS"
 
 echo "[INF] $CC version"
 "$CC" --version
@@ -23,7 +24,7 @@ echo "[INF] $CC version"
 echo "[INF] Configuring"
 ./configure --prefix="$PREFIX"
 
-if [ "$TESTS" = true ]; then
+if [ "$RUN_TESTS" = true ]; then
   echo "[INF] Running tests"
   rm -rf ./build ./"$PREFIX"
   "$MAKE" test
@@ -43,6 +44,8 @@ echo "[INF] Compressed! [$PREFIX.zip]"
 
 echo "[INF] Listing"
 tree -h "$PREFIX"
-mv "$PREFIX/$PREFIX.zip" .
+
+mkdir -p "$ARTIFACT_DIR"
+mv "$PREFIX/$PREFIX.zip" "$ARTIFACT_DIR"/
 
 echo "[INF] --- [DONE] ---"
