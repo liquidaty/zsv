d86fdc90d315dc293ed732c28f934ccad184f197
Azeem Sajid <azeem.sajid@gmail.com>
2022-05-29 12:41:48 +0500

    Refactor preparation of artifacts into a separate script.

---
+++ scripts/ci-prepare-artifacts-for-upload.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+if [ "$ARTIFACT_DIR" = "" ]; then
+  echo "[ERR] One or more environment variable(s) are not set!"
+  echo "[ERR] Set ARTIFACT_DIR before running $0 script."
+  exit 1
+fi
+
+ARTIFACT_PREFIX='zsv'
+if [ "$TAG" != "" ]; then
+  ARTIFACT_PREFIX="zsv-$TAG"
+fi
+
+echo "[INF] TAG:              $TAG"
+echo "[INF] ARTIFACT_DIR:     $ARTIFACT_DIR"
+echo "[INF] ARTIFACT_PREFIX:  $ARTIFACT_PREFIX"
+
+cd "$ARTIFACT_DIR"
+for ARTIFACT_NAME in *.zip; do
+  [ -e "$ARTIFACT_NAME" ] || break
+  UPDATED_ARTIFACT_NAME="$ARTIFACT_PREFIX-$ARTIFACT_NAME"
+  echo "$ARTIFACT_NAME => $UPDATED_ARTIFACT_NAME"
+  cp "$ARTIFACT_NAME" "$UPDATED_ARTIFACT_NAME"
+done
+cd ..
+ls -Gghl "$ARTIFACT_DIR"
+
+echo "[INF] --- [DONE] ---"
