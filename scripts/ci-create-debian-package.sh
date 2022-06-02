1a3bf1388ccd8346dcd8f6c62e297f795e12ef64
Azeem Sajid <azeem.sajid@gmail.com>
2022-06-02 10:45:48 +0500

    Create deb/rpm/brew/nuget packages via CI.

--- scripts/ci-create-debian-package.sh
+++ scripts/ci-create-debian-package.sh
@@ -8,33 +8,52 @@ if [ "$PREFIX" = "" ] || [ "$ARTIFACT_DIR" = "" ]; then
   exit 1
 fi
 
+if [ ! -d "$PREFIX" ]; then
+  echo "[ERR] PREFIX directory not found! [$PREFIX]"
+  echo "[ERR] First build with PREFIX [$PREFIX] and then run $0."
+  exit 1
+fi
+
 if [ ! -d "$ARTIFACT_DIR" ]; then
   echo "[ERR] Artifact directory not found! [$ARTIFACT_DIR]"
   exit 1
 fi
 
+ARCH="$(echo "$PREFIX" | cut -d '-' -f1)"
 VERSION="$("$PREFIX/bin/zsv" version | cut -d ' ' -f3 | cut -c2-)"
 
 echo "[INF] ARTIFACT_DIR: $ARTIFACT_DIR"
 echo "[INF] PREFIX:       $PREFIX"
+echo "[INF] ARCH:         $ARCH"
 echo "[INF] VERSION:      $VERSION"
 
-DEB="$PREFIX.deb"
-echo "[INF] Creating debian package [$DEB]"
-mkdir -p "$PREFIX/DEBIAN"
+DEBIAN_PKG="$PREFIX.deb"
+DEBIAN_DIR="$PREFIX/DEBIAN"
+DEBIAN_CONTROL_FILE="$DEBIAN_DIR/control"
 
-cat << EOF > "$PREFIX/DEBIAN/control"
+echo "[INF] Creating debian package [$DEBIAN_PKG]"
+mkdir -p "$PREFIX/usr"
+mv -f "$PREFIX/bin" "$PREFIX/include" "$PREFIX/lib" "$PREFIX/usr"
+
+mkdir -p "$DEBIAN_DIR"
+cat << EOF > "$DEBIAN_CONTROL_FILE"
 Package: zsv
 Version: $VERSION
+Section: extras
+Priority: optional
 Maintainer: Liquidaty <liquidaty@users.noreply.github.com>
-Architecture: amd64
+Architecture: $ARCH
 Description: zsv+lib: world's fastest CSV parser, with an extensible CLI
 Homepage: https://github.com/liquidaty/zsv
+Depends: libtinfo5
 EOF
 
+echo "[INF] Dumping $DEBIAN_CONTROL_FILE"
+cat "$DEBIAN_CONTROL_FILE"
+
 dpkg-deb --root-owner-group --build "$PREFIX"
-dpkg --contents "$DEB"
-ls -hl "$DEB"
-mv "$DEB" "$ARTIFACT_DIR/"
+dpkg --contents "$DEBIAN_PKG"
+ls -hl "$DEBIAN_PKG"
+mv "$DEBIAN_PKG" "$ARTIFACT_DIR/"
 
 echo "[INF] --- [DONE] ---"
