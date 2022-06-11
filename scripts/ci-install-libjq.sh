c52ea2823cea5c97b975ec2e030dc11d1b082dcc
Azeem Sajid <azeem.sajid@gmail.com>
2022-06-11 08:44:03 +0500

    Build jq from master/cff5336.

--- scripts/ci-install-libjq.sh
+++ scripts/ci-install-libjq.sh
@@ -4,25 +4,16 @@ set -e
 
 echo "[INF] Running $0"
 
-echo "[INF] Building and generating artifacts"
-
-JQ_VERSION='1.6'
-JQ_DIR='jq-1.6'
-JQ_TAR="$JQ_DIR.tar.gz"
-JQ_URL="https://github.com/stedolan/jq/releases/download/$JQ_DIR/$JQ_TAR"
+echo "[INF] Building and installing jq (libjq)"
 
 echo "[INF] PWD:              $PWD"
-echo "[INF] JQ_VERSION:       $JQ_VERSION"
-echo "[INF] JQ_DIR:           $JQ_DIR"
-echo "[INF] JQ_TAR:           $JQ_TAR"
-echo "[INF] JQ_URL:           $JQ_URL"
 
-echo "[INF] Downloading"
-wget "$JQ_URL"
-tar xvf "$JQ_TAR"
+git clone git@github.com:stedolan/jq.git
+cd jq
+git checkout cff5336
 
 echo "[INF] Configuring"
-cd "$JQ_DIR"
+autoreconf -fi
 CFLAGS='-O3' ./configure \
   --disable-maintainer-mode \
   --without-oniguruma \
@@ -31,6 +22,7 @@ CFLAGS='-O3' ./configure \
 
 echo "[INF] Building and installing"
 make install
+cd ..
 
 echo "[INF] Verifying"
 whereis jq
