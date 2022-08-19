e199a3f99f4dc54c5cb3ee578595ddb1bdb6348a
liquidaty <info@liquidaty.com>
2022-08-19 16:47:47 -0700

    initial commit

--- scripts/ci-install-libjq.sh
+++ scripts/ci-install-libjq.sh
@@ -6,6 +6,15 @@ echo "[INF] Running $0"
 
 echo "[INF] Building and installing jq (libjq)"
 
+echo "[INF] Will apply a patch to jq so that when compiling with mingw64,"
+echo "[INF] jq will not create its own functions that conflict with pthread functions"
+echo "[INF] and which in turn prevent mingw64 from statically linking libwinpthread-1.dll"
+echo "[INF] If you are building on your target system, this is not necessary, and the"
+echo "[INF] standard jq distro can be used"
+
+THIS_SCRIPT=$(readlink -f "$0")
+THIS_SCRIPT_PATH=$(dirname "$THIS_SCRIPT")
+JQ_PATCH="$THIS_SCRIPT_PATH/../app/external/jq.diff"
 JQ_GIT_URL='https://github.com/stedolan/jq.git'
 JQ_GIT_COMMIT='cff5336'
 JQ_DIR="$PWD/jq"
@@ -15,6 +24,7 @@ JQ_LIB_DIR="$JQ_PREFIX/lib"
 
 echo "[INF] PWD:              $PWD"
 echo "[INF] CC:               $CC"
+echo "[INF] JQ_PATCH:    $JQ_PATCH"
 echo "[INF] JQ_GIT_URL:       $JQ_GIT_URL"
 echo "[INF] JQ_GIT_COMMIT:    $JQ_GIT_COMMIT"
 echo "[INF] JQ_DIR:           $JQ_DIR"
@@ -27,6 +37,8 @@ rm -rf "$JQ_DIR"
 git clone "$JQ_GIT_URL"
 cd jq
 git checkout "$JQ_GIT_COMMIT"
+echo "[INF] Applying patch"
+git apply "$JQ_PATCH" || exit 1
 
 echo "[INF] Configuring"
 autoreconf -fi
@@ -39,7 +51,6 @@ if [ "$CC" = 'x86_64-w64-mingw32-gcc' ]; then
     --disable-docs \
     --disable-shared \
     --enable-static \
-    --target=win64-x86_64 \
     --host=x86_64-w64-mingw32
 else
   CFLAGS='-O3' ./configure \
