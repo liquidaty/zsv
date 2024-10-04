e3a5125d6195c9b78dd45b4351ca8496d3fdafca
liquidaty <info@liquidaty.com>
2024-10-03 23:09:25 -0700

    clang-format

--- app/ext_example/my_extension.c
+++ app/ext_example/my_extension.c
@@ -61,10 +61,10 @@ static struct zsv_ext_callbacks zsv_cb;
  * but with an additional preceding zsv_execution_context parameter.
  * Here, we just declare the functions; we fully define them further below
  */
-enum zsv_ext_status count_main(zsv_execution_context ctx, int argc, const char *argv[],
-                               struct zsv_opts *opts, const char *opts_used);
-static enum zsv_ext_status echo_main(zsv_execution_context ctx, int argc, const char *argv[],
-                                     struct zsv_opts *opts, const char *opts_used);
+enum zsv_ext_status count_main(zsv_execution_context ctx, int argc, const char *argv[], struct zsv_opts *opts,
+                               const char *opts_used);
+static enum zsv_ext_status echo_main(zsv_execution_context ctx, int argc, const char *argv[], struct zsv_opts *opts,
+                                     const char *opts_used);
 
 /**
  * *Required*. Initialization is called when our extension is loaded. Our
@@ -168,8 +168,8 @@ static void echo_rowhandler(void *ctx) {
  * `ext_xxx` functions. All we do here is initialize our data, call the parser, and
  * perform any final steps after all data has been processed
  */
-static enum zsv_ext_status echo_main(zsv_execution_context ctx, int argc, const char *argv[],
-                                     struct zsv_opts *optsp, const char *opts_used) {
+static enum zsv_ext_status echo_main(zsv_execution_context ctx, int argc, const char *argv[], struct zsv_opts *optsp,
+                                     const char *opts_used) {
   (void)(argc);
   (void)(argv);
   (void)(optsp);
@@ -241,8 +241,8 @@ static const char *count_help = "count: print the number of rows in a CSV data f
                                 "\n"
                                 "usage: count [-h,--help] [filename]\n";
 
-enum zsv_ext_status count_main(zsv_execution_context ctx, int argc, const char *argv[],
-                               struct zsv_opts *optsp, const char *opts_used) {
+enum zsv_ext_status count_main(zsv_execution_context ctx, int argc, const char *argv[], struct zsv_opts *optsp,
+                               const char *opts_used) {
   (void)(optsp);
   (void)(opts_used);
   /* help */
