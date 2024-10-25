ca4aa63758ebbc7452f40606daf04ac429e9cec2
liquidaty <info@liquidaty.com>
2024-10-24 18:57:11 -0700

    set ZSV_EXPORT to __attribute__((visibility("default"))) when compiling shared lib

--- include/zsv/zsv_export.h
+++ include/zsv/zsv_export.h
@@ -3,9 +3,13 @@
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
+#ifndef ZSV_EXPORT
 #define ZSV_EXPORT EMSCRIPTEN_KEEPALIVE
+#endif
 #else
+#ifndef ZSV_EXPORT
 #define ZSV_EXPORT
+#endif
 #endif
 
 #endif
