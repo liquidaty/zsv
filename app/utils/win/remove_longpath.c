187310c322b55c01280a7b40d98f85949e52cf6a
liquidaty <info@liquidaty.com>
2025-04-09 16:37:39 -0700

    clang-format

--- app/utils/win/remove_longpath.c
+++ app/utils/win/remove_longpath.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h> // Required for malloc, free
 #include <string.h>
-#include <wchar.h>  // Required for wide character functions
+#include <wchar.h> // Required for wide character functions
 
 #include "io.h"
 #include <errno.h>
@@ -12,33 +12,53 @@
 // return 0 if could not map
 int windows_error_to_errno(DWORD windows_error_code) {
   switch (windows_error_code) {
-  case ERROR_FILE_NOT_FOUND:    return ENOENT;
-  case ERROR_PATH_NOT_FOUND:    return ENOENT;
-  case ERROR_INVALID_DRIVE:     return ENODEV; // Or ENOENT
-    
-  case ERROR_ACCESS_DENIED:     return EACCES;
-  case ERROR_INVALID_ACCESS:    return EACCES; // Or EPERM depending on context
-  case ERROR_SHARING_VIOLATION: return EACCES; // File is locked/in use
-  case ERROR_INVALID_HANDLE:    return EBADF;
-  case ERROR_INVALID_DATA:      return EILSEQ; // Or EINVAL
-  case ERROR_INVALID_PARAMETER: return EINVAL;
-  case ERROR_NEGATIVE_SEEK:     return EINVAL;
-  case ERROR_WRITE_PROTECT:     return EROFS; // Read-only filesystem or media
-  case ERROR_DISK_FULL:         return ENOSPC; // No space left on device
-  case ERROR_ALREADY_EXISTS:    return EEXIST;
-  case ERROR_FILE_EXISTS:       return EEXIST;
-  case ERROR_TOO_MANY_OPEN_FILES: return EMFILE;
-  case ERROR_DIRECTORY:         return ENOTDIR; // Attempted file op on directory (approx)
-  case ERROR_BROKEN_PIPE:       return EPIPE;
-  case ERROR_PIPE_NOT_CONNECTED: return EPIPE;
-  case ERROR_WAIT_NO_CHILDREN:  return ECHILD; // Wait functions
-  case ERROR_CHILD_NOT_COMPLETE: return ECHILD; // Wait functions
-  case ERROR_SUCCESS:           return 0; // Not really an error
+  case ERROR_FILE_NOT_FOUND:
+    return ENOENT;
+  case ERROR_PATH_NOT_FOUND:
+    return ENOENT;
+  case ERROR_INVALID_DRIVE:
+    return ENODEV; // Or ENOENT
+
+  case ERROR_ACCESS_DENIED:
+    return EACCES;
+  case ERROR_INVALID_ACCESS:
+    return EACCES; // Or EPERM depending on context
+  case ERROR_SHARING_VIOLATION:
+    return EACCES; // File is locked/in use
+  case ERROR_INVALID_HANDLE:
+    return EBADF;
+  case ERROR_INVALID_DATA:
+    return EILSEQ; // Or EINVAL
+  case ERROR_INVALID_PARAMETER:
+    return EINVAL;
+  case ERROR_NEGATIVE_SEEK:
+    return EINVAL;
+  case ERROR_WRITE_PROTECT:
+    return EROFS; // Read-only filesystem or media
+  case ERROR_DISK_FULL:
+    return ENOSPC; // No space left on device
+  case ERROR_ALREADY_EXISTS:
+    return EEXIST;
+  case ERROR_FILE_EXISTS:
+    return EEXIST;
+  case ERROR_TOO_MANY_OPEN_FILES:
+    return EMFILE;
+  case ERROR_DIRECTORY:
+    return ENOTDIR; // Attempted file op on directory (approx)
+  case ERROR_BROKEN_PIPE:
+    return EPIPE;
+  case ERROR_PIPE_NOT_CONNECTED:
+    return EPIPE;
+  case ERROR_WAIT_NO_CHILDREN:
+    return ECHILD; // Wait functions
+  case ERROR_CHILD_NOT_COMPLETE:
+    return ECHILD; // Wait functions
+  case ERROR_SUCCESS:
+    return 0; // Not really an error
   default:
     return 0;
   }
 }
-
 
 int zsv_remove_winlp(const char *path_utf8) {
   if (path_utf8 == NULL || path_utf8[0] == '\0') {
@@ -50,7 +70,7 @@ int zsv_remove_winlp(const char *path_utf8) {
   if (!rc) {
     if (!DeleteFileW(path_to_use)) {
       DWORD lastError = GetLastError();
-      if(windows_error_to_errno(lastError))
+      if (windows_error_to_errno(lastError))
         rc = windows_error_to_errno(lastError);
       else
         rc = -1;
