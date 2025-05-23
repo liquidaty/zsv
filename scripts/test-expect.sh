775c462576388472c4c9b7f61a4b3b274409e21c
Azeem Sajid <azeem.sajid@gmail.com>
2025-05-23 11:30:40 +0500

    Use `/usr/bin/time`

--- scripts/test-expect.sh
+++ scripts/test-expect.sh
@@ -42,12 +42,12 @@ trap cleanup INT TERM QUIT
 printf "\n%s, %s" "$TARGET" "$STAGE" >>"$TIMINGS_CSV"
 
 set +e
-MATCHED_TIME=$(time -p timeout -k $((EXPECT_TIMEOUT + 1)) "$EXPECT_TIMEOUT" "$SCRIPT_DIR"/test-retry-capture-cmp.sh 2>&1)
+MATCHED_TIME=$($(which time) -p timeout -k $((EXPECT_TIMEOUT + 1)) "$EXPECT_TIMEOUT" "$SCRIPT_DIR"/test-retry-capture-cmp.sh 2>&1)
 STATUS=$?
 set -e
 
 if [ $STATUS -eq 0 ]; then
-  MATCHED_TIME=$(echo "$MATCHED_TIME" | head -n1 | cut -d' ' -f2)
+  MATCHED_TIME=$(echo "$MATCHED_TIME" | grep '^real' | cut -d' ' -f2)
   if echo "$MATCHED_TIME" | grep -qE '^[0-9]*\.?[0-9]+$' >/dev/null 2>&1; then
     MATCHED=true
     echo "$TARGET$STAGE took $MATCHED_TIME"
