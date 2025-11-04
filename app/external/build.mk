d4746a2abb81fb86bb7de98cc243312b41b96174
liquidaty <info@liquidaty.com>
2025-11-04 02:23:41 -0500

    change pcre2 build dir to BUILD_DIR-external instead of BUILD_DIR/external change Makefile to work with make -j (on macos)

--- app/external/build.mk
+++ app/external/build.mk
@@ -11,17 +11,17 @@ PCRE2_CFLAGS+=-fPIC
 
 help:
 	@echo 'make targets:'
-	@echo '  ${BUILD_DIR}/external/pcre2/lib/libpcre2-8.a'
+	@echo '  ${BUILD_DIR}-external/pcre2/lib/libpcre2-8.a'
 
 PCRE2_CONFIG_OPTIIONS=
 ifneq ($(CONFIGURE_HOST),)
   PCRE2_CONFIG_OPTIIONS+=--host=${CONFIGURE_HOST}
 endif
 
-${BUILD_DIR}/external/pcre2/lib/libpcre2-8.a:
-	@mkdir -p ${BUILD_DIR}/external/pcre2
-	@cd ${BUILD_DIR}/external/pcre2 && \
+${BUILD_DIR}-external/pcre2/lib/libpcre2-8.a:
+	@mkdir -p ${BUILD_DIR}-external/pcre2
+	@cd ${BUILD_DIR}-external/pcre2 && \
 	tar xf ${THIS_MAKEFILE_DIR_TMP}/pcre2-pcre2-10.47.tar.gz && \
 	cd pcre2-pcre2-10.47 && \
-	CFLAGS="${PCRE2_CFLAGS}" ./configure ${PCRE2_CONFIG_OPTIIONS} --prefix=${BUILD_DIR}/external/pcre2 --enable-static=yes --enable-shared=no && \
+	CFLAGS="${PCRE2_CFLAGS}" ./configure ${PCRE2_CONFIG_OPTIIONS} --prefix=${BUILD_DIR}-external/pcre2 --enable-static=yes --enable-shared=no && \
 	${MAKE} install
