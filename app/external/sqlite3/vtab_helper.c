c39e748e30ae1ece6a116a8e3e404273e96364d2
Azeem <azeem.sajid@gmail.com>
2026-01-21 11:37:49 +0500

    Fix asprintf not declared issue

--- app/external/sqlite3/vtab_helper.c
+++ app/external/sqlite3/vtab_helper.c
@@ -2,6 +2,9 @@
  * Excerpted from https://sqlite.org/src/doc/tip/ext/misc/csv.c
  */
 
+#define _GNU_SOURCE
+#include <stdio.h>
+
 /* Skip leading whitespace.  Return a pointer to the first non-whitespace
 ** character, or to the zero terminator if the string has only whitespace */
 static const char *csv_skip_whitespace(const char *z) {
