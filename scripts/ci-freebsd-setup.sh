82309f44f53829fe1b483c6fff91f05f453c66ef
Azeem <azeem.sajid@gmail.com>
2026-02-19 10:35:18 +0500

    [CI] Integrate ccache (#506)
    Enabled ccache for ci-linux, ci-musl, and ci-macos CI jobs

--- scripts/ci-freebsd-setup.sh
+++ scripts/ci-freebsd-setup.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-sudo pkg install -y tree zip git autotools gmake gcc13 tmux sqlite3 terminfo-db
+sudo pkg install -y \
+  tree \
+  zip \
+  git \
+  autotools \
+  gmake \
+  gcc13 \
+  tmux \
+  sqlite3 \
+  terminfo-db
