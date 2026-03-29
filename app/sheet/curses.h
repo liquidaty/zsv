bf2b30cfae742aaa6a9b013a3475d249712b315c
Azeem Sajid <azeem.sajid@gmail.com>
2026-03-29 15:32:04 +0500

    [MinGW] Use PDCurses instead of Ncurses
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>

--- app/sheet/curses.h
+++ app/sheet/curses.h
@@ -1,8 +1,12 @@
 #if defined(WIN32) || defined(_WIN32)
 #ifdef HAVE_NCURSESW
 #include <ncursesw/ncurses.h>
-#else
+#elif defined(HAVE_NCURSES)
 #include <ncurses/ncurses.h>
+#elif defined(HAVE_PDCURSES)
+#define PDC_WIDE
+#define set_escdelay(x) // no-op for PDCurses
+#include <curses.h>
 #endif // HAVE_NCURSESW
 #else
 #if __has_include(<ncursesw/curses.h>)
