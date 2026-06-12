63b89b11e554727edb6ded809510b6067de628dc
liquidaty <info@liquidaty.com>
2026-06-12 01:01:01 -0700

    clang-format

--- include/zsv/utils/sql.h
+++ include/zsv/utils/sql.h
@@ -5,7 +5,7 @@ typedef struct zsv_sqlite3_db *zsv_sqlite3_db_t;
 
 struct zsv_sqlite3_dbopts {
   unsigned char in_memory : 1;
-  unsigned char dedupe_cols : 1;     // auto-rename duplicate input column names (a, a_2, ...)
+  unsigned char dedupe_cols : 1;      // auto-rename duplicate input column names (a, a_2, ...)
   unsigned char warn_dupe_rename : 1; // note each such rename on stderr (off for curses UIs)
   unsigned char _ : 5;
 };
